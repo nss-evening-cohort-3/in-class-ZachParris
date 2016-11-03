c260b9a61eb1d549adeed269602723bf51d62b02
ZachParris <zacharyjparris@gmail.com>
2016-11-02 21:31:29 -0500

    part eleven in class

--- BasicsOfForms/Scripts/app.js
+++ BasicsOfForms/Scripts/app.js
@@ -1,1 +1,13 @@
 ﻿var app = angular.module("BasicsOfForms", [])
+
+app.controller("FormController", ["$scope", function ($scope) {
+    $scope.formSubmit = function (event) {
+
+        var form = $("#myform").serialize();
+        console.log();
+
+
+        event.preventDefault();
+
+    };
+}]);
