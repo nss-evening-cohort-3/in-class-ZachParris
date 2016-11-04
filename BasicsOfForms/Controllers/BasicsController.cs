2b5b3ce104e9353426a5ccbb35b09f7020a608fd
ZachParris <zacharyjparris@gmail.com>
2016-11-03 21:55:44 -0500

    hw completed

--- BasicsOfForms/Controllers/BasicsController.cs
+++ BasicsOfForms/Controllers/BasicsController.cs
@@ -32,5 +32,17 @@ namespace BasicsOfForms.Controllers
             //int i = 0;
             return View();
         }
+
+        public ActionResult Angular()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Angular(FormCollection form)
+        {
+            //int i = 0;
+            return View();
+        }
     }
 }
---
+++ BasicsOfForms/Scripts/app.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("BasicsOfForms", []);
+
+app.controller("FormController", function ($scope) {
+    
+        $scope.formSubmit = function (event) {
+                //alert("Form Submit called!");
+                var form = $("#myform").serialize();
+                console.log(form);
+        
+        
+        
+                event.preventDefault();
+            };
+});
